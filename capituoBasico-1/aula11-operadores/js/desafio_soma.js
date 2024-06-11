fdff39b3e5d5864edeed87faf9ed03a002a45b51
Lucivaldo <lucivaldojr@hotmail.com>
2024-06-11 09:02:09 -0300

    segundo exercico de fixação.

---
+++ capituoBasico-1/aula11-operadores/js/desafio_soma.js
@@ -0,0 +1,11 @@
+let num1 = prompt('Digite um número: ');
+let num2 = prompt('Digite outro número: ');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+// const result = num1 + num2;
+alert(`A soma entre esses dois números foi:  ${num1 + num2}`);
+// alert('A soma entre esses dois números foi: ' + result );
+
+// Em caso de código simples é tranquilo fazer a conta já direto no template, porém se o código for complexo pode tornar a leitura dele bastante complicada.
