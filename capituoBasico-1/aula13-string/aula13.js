c24c2a482dea7d1a3f4c4a334b95baec3d160f1e
Lucivaldo Junior <lucivaldojr@hotmail.com>
2024-06-16 09:11:00 -0300

    atualizando

---
+++ capituoBasico-1/aula13-string/aula13.js
@@ -0,0 +1,11 @@
+//        123456787 toda string tem um indice
+let umaString = "um \"texto\""; // para mostrar as aspas duplas dentro da string
+
+console.log(umaString);
+
+// maneiras de concatenação
+console.log(umaString.concat(' em um lindo dia.'));
+// console.log(umaString + ' em um lindo dia');
+// console.log(`${umaString} em um lindo dia.`);
+
+// Pra tirar mais duvidas: www.w3schools.com
